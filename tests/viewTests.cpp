575beda55accbfe52b80474ae8df854d111efc35
kamxgal <kamil.galant@gmail.com>
2018-12-22 22:38:37 +0100

    improved view tests regarding selection with predicate

--- tests/viewTests.cpp
+++ tests/viewTests.cpp
@@ -19,8 +19,13 @@ struct ViewShould : public ::testing::Test
 			auto intComp2 = std::make_shared<IntComponent>();
 			intComp2->number = 20;
 
-			entities = { 4, 10 };
-			components = { strComp1, intComp1, strComp2, intComp2 };
+			auto strComp3 = std::make_shared<StringComponent>();
+			strComp3->name = "CCC";
+			auto intComp3 = std::make_shared<IntComponent>();
+			intComp3->number = 30;
+
+			entities = { 4, 10, 17 };
+			components = { strComp1, intComp1, strComp2, intComp2, strComp3, intComp3 };
 	}
 
 	std::vector<async_ecs::entity_id> entities;
@@ -40,6 +45,11 @@ TEST_F(ViewShould, BeCreatedAndHandleGetRequests)
 	EXPECT_EQ(20, myView.select<IntComponent>(10)->number);
 	ASSERT_NE(nullptr, myView.select<StringComponent>(10));
 	EXPECT_EQ("BBB", myView.select<StringComponent>(10)->name);
+
+	ASSERT_NE(nullptr, myView.select<IntComponent>(17));
+	EXPECT_EQ(30, myView.select<IntComponent>(17)->number);
+	ASSERT_NE(nullptr, myView.select<StringComponent>(17));
+	EXPECT_EQ("CCC", myView.select<StringComponent>(17)->name);
 }
 
 TEST_F(ViewShould, BeCreatedAndHandleSelectRequestWithPredicate)
@@ -47,12 +57,17 @@ TEST_F(ViewShould, BeCreatedAndHandleSelectRequestWithPredicate)
 	async_ecs::view<StringComponent, IntComponent> myView(std::move(entities), std::move(components));
 
 	auto mapOfFounds = myView.select<IntComponent>([](auto& ptr) {
-		return ptr->number < 15;
+		return ptr->number < 15 || ptr->number > 25;
 	});
 
-	ASSERT_EQ(1, mapOfFounds.size());
-	auto& front = *mapOfFounds.begin();
-	ASSERT_EQ(4, front.first);
-	ASSERT_EQ(10, front.second->number);
-}
+	ASSERT_EQ(2, mapOfFounds.size());
+	auto iter = mapOfFounds.begin();
+	auto& firstEl = *iter;
+	ASSERT_EQ(4, firstEl.first);
+	ASSERT_EQ(10, firstEl.second->number);
+	++iter;
 
+	auto& thirdEl = *iter;
+	ASSERT_EQ(17, thirdEl.first);
+	ASSERT_EQ(30, thirdEl.second->number);
+}
